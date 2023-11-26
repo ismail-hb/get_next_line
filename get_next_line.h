9eec06aa1230663d10ec97fa39ee7a7087608344
Ismail Houche <ishouche@z2r3p2.42lyon.fr>
2023-11-27 00:36:05 +0100

    1

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/25 22:06:30 by ishouche          #+#    #+#             */
+/*   Updated: 2023/11/25 22:15:47 by ishouche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 254
+#endif
+
+# include <stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+
+#endif
