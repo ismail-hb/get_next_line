2410756205d2844e8a9e06118972085967ca5b69
Ismail Houche <ishouche@z1r11p3.42lyon.fr>
2023-11-27 22:23:24 +0100

    2

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 21:55:31 by ishouche          #+#    #+#             */
-/*   Updated: 2023/11/25 21:56:14 by ishouche         ###   ########.fr       */
+/*   Updated: 2023/11/27 22:21:05 by ishouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,12 @@
 
 char	*get_next_line(int fd)
 {
-	
+	static char	*saved_buffer[BUFFER_SIZE];
+	char 		*buffer;
+	int			i;
+
+	i = 0;
+	while (saved_buffer[i] != '\0' && saved_buffer[i] != '\n')
+		i++;
+	if ()
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,16 +6,16 @@
 /*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 22:06:30 by ishouche          #+#    #+#             */
-/*   Updated: 2023/11/25 22:15:47 by ishouche         ###   ########.fr       */
+/*   Updated: 2023/11/27 20:21:17 by ishouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE 254
-#endif
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 256
+# endif
 
 # include <stdio.h>
 # include <unistd.h>
