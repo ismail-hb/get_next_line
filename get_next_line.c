654d21b98161a35a9977c1605239a065c3b0e705
Ismail Houche <ishouche@z4r2p4.42lyon.fr>
2023-12-05 12:35:06 +0100

    realoc

--- get_next_line.c
+++ get_next_line.c
@@ -3,25 +3,88 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ismail <ismail@student.42.fr>              +#+  +:+       +#+        */
+/*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 21:55:31 by ishouche          #+#    #+#             */
-/*   Updated: 2023/11/28 16:31:04 by ismail           ###   ########.fr       */
+/*   Updated: 2023/12/05 12:30:45 by ishouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+char	*ft_strcpy(char *src, char *dest, size_t i)
+{
+	size_t	j;
+
+	j = 0;
+	while (j < i)
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	j = 0;
+	while (src[i] != '\0')
+	{
+		src[j] = src[i];
+		j++;
+		i++;
+	}
+	src[j] = '\0';
+	return(dest);
+}
+
+char	*ft_realoc(char *str, ssize_t j)
+{
+	char	*newstr;
+	size_t	i;
+
+	i = 0;
+	newstr = malloc(sizeof(char) * (BUFFER_SIZE * j));
+	while(str[i])
+	{
+		newstr[i] = str[i];
+		i++;
+	}
+	free(str);
+	newstr[i] = '\0';
+	return(newstr);
+}
+
+char	*ft_read(int fd, char **buffer, char *saved_buffer)
+{
+	ssize_t	j;
+	size_t	readvalue;
+	j = 1;
+	while (check_buff(buffer) == 0)
+	{
+		*buffer = ft_realoc(buffer, j);
+		readvalue = read(fd, buffer, BUFFER_SIZE);
+
+	}
+
+}
+
 char	*get_next_line(int fd)
 {
-	static char	*saved_buffer[BUFFER_SIZE];
+	static char	saved_buffer[BUFFER_SIZE];
 	char 		*buffer;
-	int			i;
+	char		*final;
+	ssize_t		i;
 
 	i = 0;
 	while (saved_buffer[i] != '\0' && saved_buffer[i] != '\n')
 		i++;
 	if (saved_buffer[i] == '\n')
-		return (strdup(saved_buffer, ++i));
-
+		return (ft_strcpy(saved_buffer, final, ++i));
+	if (saved_buffer[i] == '\0')
+		final = ft_read(fd, buffer, saved_buffer);
 }
+
+// int main()
+// {
+// 	char src[] = "je m'appelle isamail";
+// 	char *dest;
+// 	dest = calloc(45, sizeof(char));
+// 	printf("%s\n", ft_strcpy(src, dest, 10));
+// 	printf("%s", src);
+// }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: ishouche <ishouche@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 22:06:30 by ishouche          #+#    #+#             */
-/*   Updated: 2023/11/27 20:21:17 by ishouche         ###   ########.fr       */
+/*   Updated: 2023/12/05 08:05:52 by ishouche         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #  define BUFFER_SIZE 256
 # endif
 
+# include <stddef.h>
 # include <stdio.h>
 # include <unistd.h>
 # include <stdlib.h>
